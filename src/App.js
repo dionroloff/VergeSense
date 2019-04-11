637d586e4ad3ee163067c3948dfb58b2b353ccfc
Dion Roloff <dion.roloff@gmail.com>
2019-04-11 18:18:42 -0500

    this will add a date picker to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button, DatePicker, message } from 'antd';
 
 class App extends Component {
+
+  state = {
+    date: null,
+  };
+
+  handleClick = (event) => {
+    console.log('clicked');
+  }
+
+  handleChange = date => {
+    message.info(`Selected Date: ${date ? date.format("YYYY-MM-DD") : "None"}`);
+    this.setState({ date });
+  };
+
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
           <h1>VergeSense App</h1>
         </header>
-      </div>
-    );
+        
+        <Button type="primary" onClick={this.handleClick}>Hello</Button>
+
+        <div style={{ width: 400, margin: "100px auto" }}>
+          <DatePicker onChange={this.handleChange} />
+          <div style={{ marginTop: 20 }}>
+            Selected Date: {this.state.date ? this.state.date.format("YYYY-MM-DD") : "None"}
+          </div>
+        </div>
+
+       </div>
+    )
   }
 }
 
