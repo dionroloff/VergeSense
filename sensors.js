a026ec91ee7ee1f28bb8f28a92c9d2f2f9aa89cd
Dion Roloff <dion.roloff@gmail.com>
2019-04-11 18:47:18 -0500

    this will add getSensorReadings function to sensors.js

--- sensors.js
+++ sensors.js
@@ -1,69 +1,64 @@
-let objects = [
-    {
-      "time": "2019-03-07T19:09:24Z",
-      "sensor": "Sensor 1",
-      "number_of_people": 1
-    },
-    {
-      "time": "2019-03-07T19:14:30Z",
-      "sensor": "Sensor 1",
-      "number_of_people": 2
-    },
-    {
-      "time": "2019-03-07T19:19:35Z",
-      "sensor": "Sensor 1",
-      "number_of_people": 3
-    },
-        {
-      "time": "2019-03-07T19:09:24Z",
-      "sensor": "Sensor 2",
-      "number_of_people": 1
-    },
-    {
-      "time": "2019-03-07T19:14:30Z",
-      "sensor": "Sensor 2",
-      "number_of_people": 2
-    },
-    {
-      "time": "2019-03-07T19:19:35Z",
-      "sensor": "Sensor 3",
-      "number_of_people": 3
-    },
-  ];
-  
-  
+//this function returns the most number of people
+//each sensor reports at a given timestamp
 sensorReadings = (timestamp, data) => {
-    //the return value is an object containing the //highest number of people each sensor read at a //given point in time.
-    let results = {
-      "timestamp": timestamp,
-      "sensors": {}
-    };
-    let sensors = getSensors(data);
-  
-    sensors.forEach( (sensor) => {
-      Object.assign(results.sensors, {[sensor]: ''});
-    });
-  
-    return results;
+  //the return value is an object containing the 
+  //highest number of people each sensor reported at a given point in time.
+  let results = {
+    "timestamp": timestamp,
+    "sensors": {}
   };
-  
-//this function does not assume a number of sensors
+  //an array of each sensor (strings)
+  let sensors = getSensors(data);
+  //for each sensor in the array, we add
+  //it as a key to results.sensors
+  sensors.forEach( (sensor) => {
+    let queryString = sensor;
+    Object.assign( results.sensors, {
+        [sensor]: getMostNumberOfPeople(queryString, timestamp, data)
+      });
+  });
+
+  return results;
+};
+
+//this function does not assume a particular number of sensors
 //and will return an array of strings of each of the //sensors 
 getSensors = (data) => {
-    let sensors = [];
-    
-    //for each object in the data array,
-    //check if its "sensor" property exists
-    //in the sensors array, if not, push the string
-    data.forEach( (obj) => {
-      let sensor = obj.sensor;
-      if (!sensors.includes(sensor)) {
-        sensors.push(sensor) 
-      }
-    });
+  let sensors = [];
   
-    return sensors;
+  //for each object in the data array,
+  //check if its "sensor" property exists
+  //in the sensors array, if not, push the string
+  data.forEach( (obj) => {
+    let sensor = obj.sensor;
+    if (!sensors.includes(sensor)) {
+      sensors.push(sensor) 
+    }
+  });
+
+  return sensors;
 };
-  
-console.log(sensorReadings("2019-03-07T19:09:24Z", objects));
-  
+
+//this function returns a number of the most number
+//of people that an individual sensor reports
+//at a given timestamp
+getMostNumberOfPeople = (sensor, timestamp, data) => {
+  let mostPeople = 0;
+
+  let sensorReportAtParticularTime = [];
+  data.filter ( (obj) => {
+    if (obj.time === timestamp && obj.sensor === sensor) {
+      sensorReportAtParticularTime.push(obj);
+    } else {
+      sensorReportAtParticularTime.push({number_of_people: 0});
+    }
+  })
+
+  sensorReportAtParticularTime.forEach( (obj) => {
+    newHigh = obj.number_of_people;
+    if(newHigh > mostPeople) {
+      mostPeople = newHigh;
+    }
+  });
+  return mostPeople;
+};
--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, DatePicker, message } from 'antd';
+import { Button, 
+         DatePicker, 
+         message, 
+         Slider } from 'antd';
 
 class App extends Component {
 
   state = {
     date: null,
   };
-
-  handleClick = (event) => {
-    console.log('clicked');
-  }
 
   handleChange = date => {
     message.info(`Selected Date: ${date ? date.format("YYYY-MM-DD") : "None"}`);
@@ -22,12 +21,18 @@ class App extends Component {
       <div className="App">
         
         <header className="App-header">
-          <h1>VergeSense App</h1>
+          <h1>VergeSense</h1>
         </header>
         
-        <Button type="primary" onClick={this.handleClick}>Hello</Button>
+        {/* <Button type="primary" onClick={this.handleClick}>Hello</Button> */}
+        {/* <Slider 
+          range step={10} 
+          defaultValue={[20, 50]} 
+          onChange={onChange} 
+          onAfterChange={onAfterChange} 
+          /> */}
 
-        <div style={{ width: 400, margin: "100px auto" }}>
+        <div style={{ width: 400, margin: "50px auto" }}>
           <DatePicker onChange={this.handleChange} />
           <div style={{ marginTop: 20 }}>
             Selected Date: {this.state.date ? this.state.date.format("YYYY-MM-DD") : "None"}
